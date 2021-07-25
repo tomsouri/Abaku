a471ecd26bb5d334bb2be140d84aadcda783f9a5
Tomas Sourada <souri.tom@seznam.cz>
2021-07-25 21:40:45 +0200

    Add Formula structure.

---
+++ Abaku/Formula.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    struct Formula
+    {
+        private string formulaRepresentation { get; }
+        public int Score { get; }
+        public override string ToString()
+        {
+            return formulaRepresentation;
+        }
+        public Formula(int score, string representation)
+        {
+            formulaRepresentation = representation;
+            Score = score;
+        }
+    }
+}
