6e92578299f90fd88eb4467cafa99841062e9c93
Tomas Sourada <souri.tom@seznam.cz>
2021-07-25 21:14:10 +0200

    Add Digit structure.

---
+++ Abaku/Digit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    struct Digit
+    {
+        private byte value { get; }
+        public Digit(byte val)
+        {
+            value = val;
+        }
+        public static explicit operator Digit (int i) => new Digit((byte)i);
+        public static implicit operator int (Digit d) => d.value;
+        public static implicit operator byte (Digit d) => d.value;
+    }
+}
