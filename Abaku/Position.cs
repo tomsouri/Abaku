b2ca981aa68d0d0861aed4edd41e7a97dd086dfb
Tomas Sourada <souri.tom@seznam.cz>
2021-07-25 20:57:19 +0200

    Implement Position structure in CommonTypes.

--- Abaku/Position.cs
+++ Abaku/Position.cs
@@ -4,5 +4,21 @@ namespace CommonTypes
 {
     public struct Position
     {
+        byte Row { get; }
+        byte Column { get; }
+        public Position(byte row, byte column)
+        {
+            Row = row;
+            Column = column;
+        }
+        public Position(int row, int column)
+        {
+            Row = (byte)row;
+            Column = (byte)column;
+        }
+        public (Position, Position, Position,Position) GetAdjacentPositions()
+        {
+            return (new Position(Row + 1, Column), new Position(Row - 1, Column), new Position(Row, Column + 1), new Position(Row, Column - 1));
+        }
     }
 }
