7311125daf79e2203bb3438047c443850dbe768c
Tomas Sourada <souri.tom@seznam.cz>
2021-07-26 11:37:29 +0200

    IValidator interface.

--- BoardController/IValidator.cs
+++ BoardController/IValidator.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CommonTypes;
+
 namespace BoardController
 {
-    namespace Validator
+    interface IValidator
     {
-        interface IValidator
-        {
-        }
+        /// <summary>
+        /// Determines, whether a move is a valid move in the current situation,
+        /// that is with the current board and the concrete Formula Identifier.
+        /// </summary>
+        /// <param name="move">Move to be validated.</param>
+        /// <param name="board">Current board.</param>
+        /// <param name="formulaIdentifier">Formula Identifier to identify arrays of digits as formulas.</param>
+        /// <returns>True if the move is valid.</returns>
+        bool IsValid(Move move, IBoard board, IFormulaIdentifier formulaIdentifier);
     }
-
 }
