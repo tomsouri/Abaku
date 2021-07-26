3fccdb2e95506658937d3919d5b435a53bc6fe90
Tomas Sourada <souri.tom@seznam.cz>
2021-07-26 11:34:11 +0200

    Change accessibility of IFormulaIdentifier interface.

--- BoardController/IFormulaIdentifier.cs
+++ BoardController/IFormulaIdentifier.cs
@@ -5,35 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 using CommonTypes;
+using BoardController.OperationManager;
 
 namespace BoardController
 {
-    namespace OperationManager
+    /// <summary>
+    /// Determines, whether an array of digits is (or contains) a formula or not.
+    /// </summary>
+    interface IFormulaIdentifier
     {
+        bool IsFormula(IReadOnlyList<Digit> digits);
+
         /// <summary>
-        /// Determines, whether an array of digits is (or contains) a formula or not.
+        /// Determines, whether an array of digits contains a formula or not.
         /// </summary>
-        interface IFormulaIdentifier
-        {
-            bool IsFormula(IReadOnlyList<Digit> digits);
+        /// <param name="digits"></param>
+        /// <param name="index">Index of a digit which must be contained in the formula.</param>
+        /// <returns></returns>
+        bool ContainsFormulaIncludingIndices(IReadOnlyList<Digit> digits, int index);
 
-            /// <summary>
-            /// Determines, whether an array of digits contains a formula or not.
-            /// </summary>
-            /// <param name="digits"></param>
-            /// <param name="index">Index of a digit which must be contained in the formula.</param>
-            /// <returns></returns>
-            bool ContainsFormulaIncludingIndices(IReadOnlyList<Digit> digits, int index);
-
-            /// <summary>
-            /// Determines, whether an array of digits contains a formula or not.
-            /// </summary>
-            /// <param name="digits"></param>
-            /// <param name="index1">Index of a digit which must be contained in the formula.</param>
-            /// <param name="index2">Index of a digit which must be contained in the formula.</param>
-            /// <returns></returns>
-            bool ContainsFormulaIncludingIndices(IReadOnlyList<Digit> digits, int index1, int index2);
-        }
+        /// <summary>
+        /// Determines, whether an array of digits contains a formula or not.
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <param name="index1">Index of a digit which must be contained in the formula.</param>
+        /// <param name="index2">Index of a digit which must be contained in the formula.</param>
+        /// <returns></returns>
+        bool ContainsFormulaIncludingIndices(IReadOnlyList<Digit> digits, int index1, int index2);
     }
-
 }
