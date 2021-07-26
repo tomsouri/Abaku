f305b1b1aff221672984c690fd34951c2deafcf5
Tomas Sourada <souri.tom@seznam.cz>
2021-07-26 10:19:54 +0200

    Add comments to Move struct.

--- Abaku/Move.cs
+++ Abaku/Move.cs
@@ -6,15 +6,23 @@ using System.Threading.Tasks;
 
 namespace CommonTypes
 {
+    /// <summary>
+    /// Represents one move in a game of Abaku, that is the array of placed stones (digits) with their positions.
+    /// Supports also the move evaluation, that is adding the point score of the move.
+    /// </summary>
     public struct Move
     {
         (Digit, Position)[] PlacedStones;
-        public int Value { get; private set; }
+
+        /// <summary>
+        /// The score you get playing this move.
+        /// </summary>
+        public int Score { get; private set; }
         public bool IsEvaluated { get; private set; }
         public Move((Digit,Position)[] placedStones)
         {
             PlacedStones = placedStones;
-            Value = 0;
+            Score = 0;
             IsEvaluated = false;
         }
     }
