021871348a73d3cb8082dc901b9e686e0eed3077
Tomas Sourada <souri.tom@seznam.cz>
2021-07-26 22:25:27 +0200

    Change accessibility of methods in IEnumerablePositionExtensions, add comments.

--- BoardController/IEnumerablePositionExtensions.cs
+++ BoardController/IEnumerablePositionExtensions.cs
@@ -14,7 +14,7 @@ namespace BoardController
         {
             internal static class IEnumerablePositionExtensions
             {
-                public static bool ArePositionsPairwiseDistinct(this IEnumerable<Position> positions)
+                internal static bool ArePositionsPairwiseDistinct(this IEnumerable<Position> positions)
                 {
                     int positionsCount = 0;
                     int equalitiesCount = 0;
@@ -31,12 +31,20 @@ namespace BoardController
                     // is the same as the number of positions.
                     return positionsCount == equalitiesCount;
                 }
-                public static bool AllPositionsInSameRowOrColumn(this IEnumerable<Position> positions)
+                internal static bool AllPositionsInSameRowOrColumn(this IEnumerable<Position> positions)
                 {
                     return positions.AllEqual(equalityComparison: Position.HaveSameRow) ||
                             positions.AllEqual(equalityComparison: Position.HaveSameColumn);
                 }
-                public static bool AllEqual<Position>(this IEnumerable<Position> positions, Func<Position, Position, bool> equalityComparison)
+
+                /// <summary>
+                /// Are all Positions in the collection equal, compared using delegate equalityComparison?
+                /// </summary>
+                /// <typeparam name="Position"></typeparam>
+                /// <param name="positions"></param>
+                /// <param name="equalityComparison"></param>
+                /// <returns>True if all Positions are equal.</returns>
+                internal static bool AllEqual<Position>(this IEnumerable<Position> positions, Func<Position, Position, bool> equalityComparison)
                 {
                     if (!positions.Any()) return true;
                     Position first = positions.First();
--- BoardController/Validator.cs
+++ BoardController/Validator.cs
@@ -97,7 +97,6 @@ namespace BoardController
 
         }
 
-
     }
 
 }
