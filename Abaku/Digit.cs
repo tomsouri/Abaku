d100c535fb08ee612500af2ebc4ae0bc92841b50
Tomas Sourada <souri.tom@seznam.cz>
2021-07-26 10:17:31 +0200

    Add comments to Digit struct, as well as range check.

--- Abaku/Digit.cs
+++ Abaku/Digit.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace CommonTypes
 {
+    /// <summary>
+    /// Represents a valid digit in a decimal system, that is, can contain bytes 0, 1, ..., 9.
+    /// </summary>
     public struct Digit
     {
         private byte value { get; }
         public Digit(byte val)
         {
+            if (val >= 10)
+            {
+                throw new ArgumentOutOfRangeException("The argument " + nameof(val) + " must be a valid decimal digit");
+            }
             value = val;
         }
         public static explicit operator Digit (int i) => new Digit((byte)i);
