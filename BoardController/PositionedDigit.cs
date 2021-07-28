41dad9fab773dff583f66c0ca82e9f6a65b0e940
Tomas Sourada <souri.tom@seznam.cz>
2021-07-28 14:47:07 +0200

    Add structure PositionedDigit, which is going to be used during Evaluation.

---
+++ BoardController/PositionedDigit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommonTypes;
+
+namespace BoardController
+{
+    namespace EvaluationManager
+    {
+        internal struct PositionedDigit
+        {
+            public Digit digit { get; }
+            public Position position { get; }
+            public bool IsNewlyPlaced { get; }
+        }
+    }
+
+}
