ac6e5fb9942dbcdcb6825f9ec2f2020722bcc860
Tomas Sourada <souri.tom@seznam.cz>
2021-07-28 14:55:34 +0200

    Add EvaluationManager class with basic methods..

---
+++ BoardController/EvaluationManager.cs
@@ -0,0 +1,40 @@
+﻿using CommonTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardController
+{
+    namespace EvaluationManager
+    {
+        class EvaluationManager : IEvaluationManager
+        {
+            private FormulaEvaluationDelegate CurrentFormulaEvaluation { get; set; }
+
+            //private IEnumerable<>
+
+            public int Evaluate(Move move, IBoard board, IFormulaIdentifier formulaIdentifier)
+            {
+                throw new NotImplementedException();
+            }
+
+            public int Evaluate(Move move, IBoard board, IFormulaIdentifier formulaIdentifier, FormulaEvaluationDelegate formulaEvaluation)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<FormulaRepresentation> GetAllFormulasIncludedIn(Move move, IBoard board, IFormulaIdentifier formulaIdentifier)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<FormulaRepresentation> GetAllFormulasIncludedIn(Move move, IBoard board, IFormulaIdentifier formulaIdentifier, FormulaEvaluationDelegate formulaEvaluation)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
