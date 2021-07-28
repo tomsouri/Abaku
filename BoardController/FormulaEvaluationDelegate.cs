f4051916049dfb7cfc7e8ca4824b8846f92ce11f
Tomas Sourada <souri.tom@seznam.cz>
2021-07-28 14:48:07 +0200

    Add delegate FormulaEvaluationDelegate.

---
+++ BoardController/FormulaEvaluationDelegate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardController
+{
+    namespace EvaluationManager
+    {
+        delegate int FormulaEvaluationDelegate(IEnumerable<PositionedDigit> formula);
+    }
+
+}
