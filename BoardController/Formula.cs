843209b5c846d0f86d508d9f3a361bdd578b2f0a
Tomas Sourada <souri.tom@seznam.cz>
2021-07-28 14:59:22 +0200

    Add Formula structure, which is going to be used for formula evaluation.

---
+++ BoardController/Formula.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommonTypes;
+
+namespace BoardController
+{
+    namespace EvaluationManager
+    {
+        struct Formula : IEnumerable<PositionedDigit>
+        {
+            private Position Start { get; }
+            private Position End { get; }
+            private IBoardAfterHypotheticalMove BoardAfterMove {get;}
+            public IEnumerator<PositionedDigit> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+
+}
