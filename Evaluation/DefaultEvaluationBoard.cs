4ef79ecb814fb8ef651985bdee0548ec0c3a6efb
Tomas Sourada <souri.tom@seznam.cz>
2021-07-29 18:41:56 +0200

    Add DefaultEvalBoard class.

---
+++ Evaluation/DefaultEvaluationBoard.cs
@@ -0,0 +1,14 @@
+﻿using CommonTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation
+{
+    internal class DefaultEvaluationBoard : IEvaluationBoard
+    {
+        public PositionEvaluationInfo this[Position position] => throw new NotImplementedException();
+    }
+}
