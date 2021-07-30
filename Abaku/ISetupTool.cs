a75dd8a0dc18d4127914a7aa65470d6b88ddf073
Tomas Sourada <souri.tom@seznam.cz>
2021-07-30 13:09:02 +0200

    Add ISetupTool interface to CommonTypes.

---
+++ Abaku/ISetupTool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    /// <summary>
+    /// Is used in communication between the game environment and the BoardController.
+    /// </summary>
+    interface ISetupTool
+    {
+        /// <summary>
+        /// The description to display in the settings.
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// The method to setup using of the specific setting.
+        /// </summary>
+        void Setup();
+    }
+}
