9b8aba45a23437d0cdd37b61d35f74c0641fb898
Tomas Sourada <souri.tom@seznam.cz>
2021-07-30 13:29:15 +0200

    Implement constructor of BoardController.

--- BoardController/BoardController.cs
+++ BoardController/BoardController.cs
@@ -13,28 +13,68 @@ using Optimizing;
 
 namespace BoardController
 {
-    internal class BoardController : IBoardController
+    public class BoardController : IBoardController
     {
-        public int EnterMove(Move move)
+        private IValidator Validator { get; }
+        private IEvaluationManager EvalManager { get; }
+        private IOperationManager OpManager { get; }
+        private IBoardManager BoardManager { get; }
+        private IOptimizer Optimizer { get; }
+        public BoardController()
+        {
+            Validator = Validation.Validator.Instance;
+            EvalManager = new EvaluationManager();
+            OpManager = new OperationManager();
+            BoardManager = new BoardManaging.BoardManager();
+            Optimizer = new Optimizing.Optimizer();
+        }
+        bool IBoardController.IsValid(Move move)
+        {
+            throw new NotImplementedException();
+        }
+        int IBoardController.Evaluate(Move move)
+        {
+            throw new NotImplementedException();
+        }
+        IReadOnlyList<FormulaRepresentation> IBoardController.AllFormulasIncludedIn(Move move)
         {
             throw new NotImplementedException();
         }
 
-        public IReadOnlyList<Move> GetBestMoves(IReadOnlyList<Digit> availableStones)
+        int IBoardController.EnterMove(Move move)
         {
             throw new NotImplementedException();
         }
 
-        // TODO: mozna se zmeni a bude vracet ReadOnly 2D array, ktere implementujeme.
-        // spis ne, vracenim kopie neprislis casto nevznika tlak na GC-heap.
-        public Digit?[,] GetCurrentStateOfBoard()
+        IReadOnlyList<Move> IBoardController.GetBestMoves(IReadOnlyList<Digit> availableStones)
+        {
+            throw new NotImplementedException();
+        }
+        void IBoardController.EnterMoveUnsafe(Move move)
+        {
+            throw new NotImplementedException();
+        }
+        IReadOnlyList<ISetupTool> IBoardController.GetOperationSetupTools()
         {
             throw new NotImplementedException();
         }
 
-        public IReadOnlyList<FormulaRepresentation> AllFormulasIncludedIn(Move move)
+        IReadOnlyList<ISetupTool> IBoardController.GetEvaluationSetupTools()
         {
             throw new NotImplementedException();
         }
+
+        IReadOnlyList<ISetupTool> IBoardController.GetBoardSetupTools()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
+        Digit?[,] IBoardController.GetBoardContent()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
