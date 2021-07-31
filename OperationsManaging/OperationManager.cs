ab557ad03ce7239bee533751a7bd2ca7e536fe95
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 18:30:46 +0200

    Implement GetOpSetupTools in OpMger and nested class AllOperationsIfiers.

--- OperationsManaging/OperationManager.cs
+++ OperationsManaging/OperationManager.cs
@@ -39,8 +39,22 @@ namespace OperationsManaging
 
         public IReadOnlyList<ISetupTool> GetOperationSetupTools()
         {
-            throw new NotImplementedException();
+            var list = new List<ISetupTool>();
+            foreach (var identifier in AllOperationsIdentifiers.SimpleIdentifiers)
+            {
+                list.Add(new OperationSetupTool(identifier, this.FactorsIdentifier.Add));
+            }
+            return list;
         }
 
+
+        internal static class AllOperationsIdentifiers
+        {
+            public static IEnumerable<ISimpleFactorsFormulaIdentifier> SimpleIdentifiers { get; }
+            public static IReadOnlyList<BinaryIdentifier> BinaryIdentifiers { get; }
+            public static IReadOnlyList<UnaryIdentifier> UnaryIdentifiers { get; }
+
+            // TODO: implement lists of identifiers
+        }
     }
 }
