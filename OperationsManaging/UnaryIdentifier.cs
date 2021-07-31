2cac9e1e7eea1f12775def678c43dc69264cfe0e
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 19:27:00 +0200

    Take back the last commit.

--- OperationsManaging/UnaryIdentifier.cs
+++ OperationsManaging/UnaryIdentifier.cs
@@ -23,4 +23,26 @@ namespace OperationsManaging
 
         public abstract bool IsFormula(long aFactor, long bFactor);
     }
+
+    internal delegate long
+
+    internal sealed class TUnaryIdentifier : ISimpleFactorsFormulaIdentifier
+    {
+        public static int Arity => 1;
+        public int OperatorArity => Arity;
+
+        
+        public string Description { get; }
+
+        public bool IsFormula(long aFactor, long bFactor, long cFactor) => false;
+
+        public bool IsFormula(long[] factors)
+        {
+            if (factors.Length != Arity + 1) return false;
+            return IsFormula(factors[0], factors[1]);
+        }
+
+        public bool IsFormula(long aFactor, long bFactor) =>;
+    }
+
 }
