e37ef12bf06c6e03681ed178dd57d84d53502e2e
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 11:36:14 +0200

    Add OperationSetupTool to OpMger.

--- OperationsManaging/OperationManager.cs
+++ OperationsManaging/OperationManager.cs
@@ -8,9 +8,22 @@ namespace OperationsManaging
     {
         public IFormulaIdentifier FormulaIdentifier => throw new NotImplementedException();
 
+
+
+        private class OperationSetupTool : ISetupTool
+        {
+            public string Description => throw new NotImplementedException();
+
+            public void Setup()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         public IReadOnlyList<ISetupTool> GetOperationSetupTools()
         {
             throw new NotImplementedException();
         }
+
     }
 }
