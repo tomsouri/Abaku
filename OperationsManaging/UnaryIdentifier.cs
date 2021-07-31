e535e359f1cc0a99629d9b5d8e59c3c67431aaf5
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 11:46:35 +0200

    Add UnaryIdentifier abstract class.

---
+++ OperationsManaging/UnaryIdentifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsManaging
+{
+    internal abstract class UnaryIdentifier : ISimpleFactorsFormulaIdentifier
+    {
+        public int Arity => 1;
+
+        
+        public bool IsFormula(long aFactor, long bFactor, long cFactor) => false;
+
+        public bool IsFormula(long[] factors)
+        {
+            if (factors.Length != Arity + 1) return false;
+            return IsFormula(factors[0], factors[1]);
+        }
+
+        public abstract bool IsFormula(long aFactor, long bFactor);
+    }
+}
