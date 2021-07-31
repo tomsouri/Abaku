ae556f75b0d8b90370cb9a96e9681401190abcc3
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 19:29:55 +0200

    Implement UnaryIdentifier in the same way as BinaryIdentifier.

--- OperationsManaging/UnaryIdentifier.cs
+++ OperationsManaging/UnaryIdentifier.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OperationsManaging
 {
-    internal abstract class UnaryIdentifier : ISimpleFactorsFormulaIdentifier
+    /*internal abstract class UnaryIdentifier : ISimpleFactorsFormulaIdentifier
     {
         public static int Arity => 1;
         public int OperatorArity => Arity;
@@ -22,16 +22,21 @@ namespace OperationsManaging
         }
 
         public abstract bool IsFormula(long aFactor, long bFactor);
-    }
+    }*/
 
-    internal delegate long
+    internal delegate bool UnaryOperationDelegate(long factor, long result);
 
-    internal sealed class TUnaryIdentifier : ISimpleFactorsFormulaIdentifier
+    internal sealed class UnaryIdentifier : ISimpleFactorsFormulaIdentifier
     {
+        public UnaryIdentifier(string description, UnaryOperationDelegate operationDelegate)
+        {
+            Description = description;
+            OperationDelegate = operationDelegate;
+        }
         public static int Arity => 1;
         public int OperatorArity => Arity;
 
-        
+        private UnaryOperationDelegate OperationDelegate { get; }
         public string Description { get; }
 
         public bool IsFormula(long aFactor, long bFactor, long cFactor) => false;
@@ -42,7 +47,7 @@ namespace OperationsManaging
             return IsFormula(factors[0], factors[1]);
         }
 
-        public bool IsFormula(long aFactor, long bFactor) =>;
+        public bool IsFormula(long factor, long result) => OperationDelegate(factor,result);
     }
 
 }
