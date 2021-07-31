ab8288727b40ccd73fa435fe31a500b4ba079c77
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 11:42:59 +0200

    Add IUniversalFactorsFlaIfier.

---
+++ OperationsManaging/IUniversalFactorsFormulaIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsManaging
+{
+    internal interface IUniversalFactorsFormulaIdentifier : IFactorsFormulaIdentifier
+    {
+        IEnumerable<int> Arities { get; }
+        void Add(ISimpleFactorsFormulaIdentifier simpleIdentifier);
+
+    }
+}
