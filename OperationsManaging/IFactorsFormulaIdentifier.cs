9ff5c400665a4555936ebc8a44f3f1909db4d225
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 11:39:45 +0200

    Add IFactorsFormulaIdentifier interface.

---
+++ OperationsManaging/IFactorsFormulaIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsManaging
+{
+    internal interface IFactorsFormulaIdentifier
+    {
+        bool IsFormula(long aFactor, long bFactor);
+        bool IsFormula(long aFactor, long bFactor, long cFactor);
+        bool IsFormula(long[] factors);
+    }
+}
