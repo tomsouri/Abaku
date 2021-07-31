ff4308062afefd4b3f3447d29e00ed987db977d5
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 20:44:14 +0200

    Implement all simple identifiers in AllOperationsIdentifiers.

--- OperationsManaging/OperationManager.cs
+++ OperationsManaging/OperationManager.cs
@@ -52,20 +52,43 @@ namespace OperationsManaging
         {
             public static IEnumerable<ISimpleFactorsFormulaIdentifier> SimpleIdentifiers
             {
-                get
-                {
-                    return (UnaryIdentifiers, BinaryIdentifiers, OtherIdentifiers).Combine();
-                }
+                get => (UnaryIdentifiers, BinaryIdentifiers, OtherIdentifiers).Combine();
             }
-            public static IReadOnlyList<BinaryIdentifier> BinaryIdentifiers { get; }
-            public static IReadOnlyList<UnaryIdentifier> UnaryIdentifiers { get; }
-            public static IReadOnlyList<ISimpleFactorsFormulaIdentifier> OtherIdentifiers { get; }
+            public static IReadOnlyList<BinaryIdentifier> BinaryIdentifiers => binaryIdentifiers;
+            public static IReadOnlyList<UnaryIdentifier> UnaryIdentifiers => unaryIdentifiers;
+            public static IReadOnlyList<ISimpleFactorsFormulaIdentifier> OtherIdentifiers => otherIdentifiers;
+            private static List<UnaryIdentifier> unaryIdentifiers { get; }
+            private static List<BinaryIdentifier> binaryIdentifiers { get; }
+            private static List<ISimpleFactorsFormulaIdentifier> otherIdentifiers { get; }
             static AllOperationsIdentifiers()
             {
+                var unaryDescriptionsAndLambdas = new List<(string description, UnaryOperationDelegate operation)>()
+                {
+                    ("Square root", (long factor, long result) => result*result == factor),
+                    ("Cube root", (long factor, long result) => result*result*result == factor),
+                    ("Second power", (long factor, long result) => factor*factor == result),
+                    ("Third power", (long factor, long result) => factor*factor*factor == result)
+                };
+                var binaryDescriptionsAndLambdas = new List<(string description, BinaryOperationDelegate operation)>()
+                {
+                    ("Addition", (long a, long b, long result) => a + b == result),
+                    ("Subtraction", (long a, long b, long result) => a - b == result),
+                    ("Multiplication", (long a, long b, long result) => a * b == result),
+                    ("Division", (long a, long b, long result) => a == b * result)
+                };
+                unaryIdentifiers = new List<UnaryIdentifier>();
+                binaryIdentifiers = new List<BinaryIdentifier>();
+                otherIdentifiers = new List<ISimpleFactorsFormulaIdentifier>();
 
+                foreach (var x in unaryDescriptionsAndLambdas)
+                {
+                    unaryIdentifiers.Add(new UnaryIdentifier(x.description, x.operation));
+                }
+                foreach (var x in binaryDescriptionsAndLambdas)
+                {
+                    binaryIdentifiers.Add(new BinaryIdentifier(x.description, x.operation));
+                }
             }
-
-            // TODO: implement lists of identifiers
         }
     }
     
