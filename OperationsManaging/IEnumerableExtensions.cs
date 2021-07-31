f17bd5a310e6f0388a4d2579fa0cc7ec49d36c25
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 19:41:00 +0200

    Implement IEnumerableExtensions for combining multiple enumerables.

---
+++ OperationsManaging/IEnumerableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsManaging
+{
+    internal static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Combine<T>(this (IEnumerable<T> first, IEnumerable<T> second) enumerables)
+        {
+            foreach (var item in enumerables.first)
+            {
+                yield return item;
+            }
+            foreach (var item in enumerables.second)
+            {
+                yield return item;
+            }
+        }
+        public static IEnumerable<T> Combine<T>(this (IEnumerable<T> first, IEnumerable<T> second, IEnumerable<T> third) enumerables)
+        {
+            foreach (var item in enumerables.first)
+            {
+                yield return item;
+            }
+            foreach (var item in enumerables.second)
+            {
+                yield return item;
+            }
+            foreach (var item in enumerables.third)
+            {
+                yield return item;
+            }
+        }
+    }
+}
