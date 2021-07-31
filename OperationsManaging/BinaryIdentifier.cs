9d2e42cb0cd57b815da62922cff4bef49f965164
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 11:48:25 +0200

    Add BinaryIdentifier.

---
+++ OperationsManaging/BinaryIdentifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsManaging
+{
+    internal abstract class BinaryIdentifier : ISimpleFactorsFormulaIdentifier
+    {
+        public int Arity => 2;
+
+        public bool IsFormula(long aFactor, long bFactor) => false;
+
+        public abstract bool IsFormula(long aFactor, long bFactor, long cFactor);
+
+        public bool IsFormula(long[] factors)
+        {
+            if (factors.Length != Arity + 1) return false;
+            return IsFormula(factors[0], factors[1], factors[2]);
+        }
+    }
+}
