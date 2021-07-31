92c0accf1ee197c8f67a0540ff845bf1369f9003
Tomas Sourada <souri.tom@seznam.cz>
2021-07-31 12:03:22 +0200

    Add TODO comments to methods in UniFactorsFlaIfier.

--- OperationsManaging/UniversalFactorsFormulaIdentifier.cs
+++ OperationsManaging/UniversalFactorsFormulaIdentifier.cs
@@ -8,6 +8,15 @@ namespace OperationsManaging
 {
     internal class UniversalFactorsFormulaIdentifier : IUniversalFactorsFormulaIdentifier
     {
+        public UniversalFactorsFormulaIdentifier()
+        {
+            HasDefaultSetup = true;
+
+            // TODO: add default setup of identifiers.
+
+            throw new NotImplementedException();
+        }
+        private bool HasDefaultSetup { get; set; }
         private List<int> KnownArities { get; set; }
         public IEnumerable<int> Arities => KnownArities;
         private List<UnaryIdentifier> UnaryIdentifiers { get; }
@@ -15,21 +24,29 @@ namespace OperationsManaging
 
         public void Add(IFactorsFormulaIdentifier identifier)
         {
+            // TODO: check, if it has default setup.
+            // if so, clear all identifier lists and add this one.
+            // if not, just add this one.
+
             throw new NotImplementedException();
         }
 
         public bool IsFormula(long aFactor, long bFactor)
         {
+            // call all identifiers with specific arity
+
             throw new NotImplementedException();
         }
 
         public bool IsFormula(long aFactor, long bFactor, long cFactor)
         {
+            // call all identifiers with specific arity
             throw new NotImplementedException();
         }
 
         public bool IsFormula(long[] factors)
         {
+            // call all identifiers with specific arity
             throw new NotImplementedException();
         }
     }
