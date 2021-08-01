a3d7111af7ebd70f93c13e7e606ab38fb04c478b
Tomas Sourada <souri.tom@seznam.cz>
2021-08-01 16:58:00 +0200

    Add CombineWith extension method to IEnumerableExtensions.

--- Extensions/EnumerableCombineExtensions.cs
+++ Extensions/EnumerableCombineExtensions.cs
@@ -5,31 +5,24 @@ namespace EnumerableCombineExtensions
 {
     public static class IEnumerableExtensions
     {
-        public static IEnumerable<T> Combine<T>(this (IEnumerable<T> first, IEnumerable<T> second) enumerables)
+        public static IEnumerable<T> CombineWith<T> (this IEnumerable<T> first, IEnumerable<T> second)
         {
-            foreach (var item in enumerables.first)
+            foreach (var item in first)
             {
                 yield return item;
             }
-            foreach (var item in enumerables.second)
+            foreach (var item in second)
             {
                 yield return item;
             }
         }
+        public static IEnumerable<T> Combine<T>(this (IEnumerable<T> first, IEnumerable<T> second) enumerables)
+        {
+            return enumerables.first.CombineWith(enumerables.second);
+        }
         public static IEnumerable<T> Combine<T>(this (IEnumerable<T> first, IEnumerable<T> second, IEnumerable<T> third) enumerables)
         {
-            foreach (var item in enumerables.first)
-            {
-                yield return item;
-            }
-            foreach (var item in enumerables.second)
-            {
-                yield return item;
-            }
-            foreach (var item in enumerables.third)
-            {
-                yield return item;
-            }
+            return enumerables.first.CombineWith(enumerables.second).CombineWith(enumerables.third);
         }
     }
 }
