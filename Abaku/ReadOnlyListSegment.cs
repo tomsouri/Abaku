bce27569276b797fcc1b94a0892576173c86fe8d
Tomas Sourada <souri.tom@seznam.cz>
2021-08-02 20:29:39 +0200

    Add ReadOnlyListSegment struct to CommonTypes as an equivalent to ArraySegment for IReadOnlyLists.

---
+++ Abaku/ReadOnlyListSegment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    public struct ReadOnlyListSegment<T> : IReadOnlyList<T>
+    {
+        public ReadOnlyListSegment(IReadOnlyList<T> list, int startIndex, int count)
+        {
+            throw new NotImplementedException();
+        }
+        public T this[int index] => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
