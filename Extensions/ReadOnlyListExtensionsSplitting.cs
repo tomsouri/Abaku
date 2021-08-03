285c6636801134dbbb1f478a8100f0940fd2f8f3
Tomas Sourada <souri.tom@seznam.cz>
2021-08-03 12:38:36 +0200

    Implement SplitIntoTwoParts method in SplitReadOnlyListExtensions.

--- Extensions/ReadOnlyListExtensionsSplitting.cs
+++ Extensions/ReadOnlyListExtensionsSplitting.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CommonTypes;
+
 namespace ReadOnlyListExtensions
 {
     namespace Splitting
     {
         public static class SplitReadOnlyListExtensions
         {
-
+            public static IEnumerable<(IReadOnlyList<T>, IReadOnlyList<T>)> SplitIntoTwoParts<T>(IReadOnlyList<T> list)
+            {
+                for (int firstCount = 1; firstCount < list.Count; firstCount++)
+                {
+                    yield return   (new ReadOnlyListSegment<T>(list, startIndex:0, firstCount), 
+                        new ReadOnlyListSegment<T>(list, startIndex:firstCount, count: list.Count - firstCount));
+                }
+            }
+            public static IEnumerable<(IReadOnlyList<T>, IReadOnlyList<T>, IReadOnlyList<T>)> SplitToThreeParts<T>(IReadOnlyList<T> list)
+            {
+                throw new NotImplementedException();
+            }
+            public static IEnumerable<IReadOnlyList<T>[]> SplitToParts<T>(IReadOnlyList<T> list, int partsCount)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
