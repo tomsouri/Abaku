c62624bc847ace9adb9e31c918ab157dd91aedd3
Tomas Sourada <souri.tom@seznam.cz>
2021-08-03 12:46:57 +0200

    Add methods.

--- OperationsManaging/FormulaIdentifier.cs
+++ OperationsManaging/FormulaIdentifier.cs
@@ -5,16 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ReadOnlyListExtensions;
+using ReadOnlyListDigitExtensions;
+
 namespace OperationsManaging
 {
     internal class FormulaIdentifier : IFormulaIdentifier
     {
+        /// <summary>
+        /// Identifier used to identify, whether a tuple (or array) of factors
+        /// represents a valid formula or not, and to get the formula string representation.
+        /// </summary>
         private IUniversalFactorsFormulaIdentifier FactorsIdentifier { get; }
         public FormulaIdentifier(IUniversalFactorsFormulaIdentifier factorsIdentifier)
         {
             FactorsIdentifier = factorsIdentifier;
         }
-        public bool ContainsFormulaIncludingIndices(IReadOnlyList<Digit> digits, int index)
+        public bool ContainsFormulaIncludingIndex(IReadOnlyList<Digit> digits, int index)
         {
             throw new NotImplementedException();
         }
@@ -29,9 +36,21 @@ namespace OperationsManaging
             throw new NotImplementedException();
         }
 
+        private string GetFormulaString(IReadOnlyList<Digit> digits, int arity)
+        {
+            throw new NotImplementedException();
+        }
+
+
         public bool IsFormula(IReadOnlyList<Digit> digits)
         {
             throw new NotImplementedException();
         }
+
+        private bool IsFormula(IReadOnlyList<Digit> digits, int arity)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- OperationsManaging/IFormulaIdentifier.cs
+++ OperationsManaging/IFormulaIdentifier.cs
@@ -18,14 +18,15 @@ namespace OperationsManaging
         /// <param name="digits"></param>
         /// <param name="index">Index of a digit which must be contained in the formula.</param>
         /// <returns></returns>
-        bool ContainsFormulaIncludingIndices(IReadOnlyList<Digit> digits, int index);
+        bool ContainsFormulaIncludingIndex(IReadOnlyList<Digit> digits, int index);
 
         /// <summary>
         /// Determines, whether an array of digits contains a formula or not.
+        /// Works also for index1=index2.
         /// </summary>
         /// <param name="digits"></param>
         /// <param name="index1">Index of a digit which must be contained in the formula.</param>
-        /// <param name="index2">Index of a digit which must be contained in the formula.</param>
+        /// <param name="index2">Other index of a digit which must be contained in the formula.</param>
         /// <returns></returns>
         bool ContainsFormulaIncludingIndices(IReadOnlyList<Digit> digits, int index1, int index2);
 
