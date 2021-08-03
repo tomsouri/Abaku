baffabcf28ea53712f6673d359d47d2c8c61b9af
Tomas Sourada <souri.tom@seznam.cz>
2021-08-03 10:16:38 +0200

    Add OperationString property to UnaryIfier and implement GetFormulaString methods using it.

--- OperationsManaging/UnaryIdentifier.cs
+++ OperationsManaging/UnaryIdentifier.cs
@@ -10,15 +10,18 @@ namespace OperationsManaging
 
     internal sealed class UnaryIdentifier : ISimpleFactorsFormulaIdentifier
     {
-        public UnaryIdentifier(string description, UnaryOperationDelegate operationDelegate)
+        public UnaryIdentifier(string description, UnaryOperationDelegate operationDelegate, string operationString)
         {
             Description = description;
             OperationDelegate = operationDelegate;
+            OperationString = operationString;
         }
         public static int Arity => 1;
         public int OperatorArity => Arity;
 
         private UnaryOperationDelegate OperationDelegate { get; }
+        private string OperationString { get; }
+        private static string EqualityString = "=";
         public string Description { get; }
 
         public bool IsFormula(long aFactor, long bFactor, long cFactor) => false;
@@ -30,6 +33,23 @@ namespace OperationsManaging
         }
 
         public bool IsFormula(long factor, long result) => OperationDelegate(factor,result);
+
+        public string GetFormulaString(long factor, long result)
+        {
+            if (!IsFormula(factor, result)) return null;
+            return factor.ToString() + OperationString + EqualityString + result.ToString();
+        }
+
+        public string GetFormulaString(long aFactor, long bFactor, long result)
+        {
+            return null;
+        }
+
+        public string GetFormulaString(long[] factors)
+        {
+            if (factors.Length != Arity + 1) return null;
+            return GetFormulaString(factors[0], factors[1]);
+        }
     }
 
 }
