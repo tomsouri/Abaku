38c4cb8cb2485e77e94466da406b3e6c3f5b1fd4
Tomas Sourada <souri.tom@seznam.cz>
2021-08-03 11:52:46 +0200

    Implement ToLong method using Horner schema in ReadOnlyListDigitExtensions.

---
+++ Extensions/ReadOnlyListDigitExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommonTypes;
+
+namespace ReadOnlyListDigitExtensions
+{
+    public static class ReadOnlyListDigitExtensions
+    {
+        private static int BASE = 10;
+        /// <summary>
+        /// Converts list of digits to long representing the string of digits
+        /// using Horner schema in decimal system.
+        /// </summary>
+        /// <param name="digits">List of digits to convert.</param>
+        /// <returns>Long, the converted number.</returns>
+        public static long ToLong(this IReadOnlyList<Digit> digits)
+        {
+            long result = 0;
+            for (int i = 0; i < digits.Count; i++)
+            {
+                result *= BASE;
+                result += digits[i];
+            }
+            return result;
+        }
+    }
+}
