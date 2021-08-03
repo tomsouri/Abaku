3ee694515fa1fc1ee801205e12b35116983aaedf
Tomas Sourada <souri.tom@seznam.cz>
2021-08-03 12:43:43 +0200

    Implement SplitIntoThreeParts method in SplitROLExtensions.

--- Extensions/ReadOnlyListExtensionsSplitting.cs
+++ Extensions/ReadOnlyListExtensionsSplitting.cs
@@ -16,13 +16,22 @@ namespace ReadOnlyListExtensions
             {
                 for (int firstCount = 1; firstCount < list.Count; firstCount++)
                 {
-                    yield return   (new ReadOnlyListSegment<T>(list, startIndex:0, firstCount), 
+                    yield return (new ReadOnlyListSegment<T>(list, startIndex:0, firstCount), 
                         new ReadOnlyListSegment<T>(list, startIndex:firstCount, count: list.Count - firstCount));
                 }
             }
             public static IEnumerable<(IReadOnlyList<T>, IReadOnlyList<T>, IReadOnlyList<T>)> SplitToThreeParts<T>(IReadOnlyList<T> list)
             {
-                throw new NotImplementedException();
+                for (int  firstCount = 1;  firstCount < list.Count - 1;  firstCount++)
+                {
+                    for (int secondCount = 1; firstCount + secondCount < list.Count; secondCount++)
+                    {
+                        var thirdCount = list.Count - firstCount - secondCount;
+                        yield return (new ReadOnlyListSegment<T>(list, startIndex: 0, firstCount),
+                            new ReadOnlyListSegment<T>(list, startIndex: firstCount, secondCount),
+                            new ReadOnlyListSegment<T>(list, startIndex: firstCount + secondCount, thirdCount));
+                    }
+                }
             }
             public static IEnumerable<IReadOnlyList<T>[]> SplitToParts<T>(IReadOnlyList<T> list, int partsCount)
             {
