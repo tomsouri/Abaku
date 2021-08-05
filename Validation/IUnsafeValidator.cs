4b9617ff75f1df32b3488132f140950b2ff7fd3f
Tomas Sourada <souri.tom@seznam.cz>
2021-08-05 20:48:47 +0200

    Implement interface IUnsageValidator.

--- Validation/IUnsafeValidator.cs
+++ Validation/IUnsafeValidator.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OperationsManaging;
+using BoardManaging;
+using CommonTypes;
+
 namespace Validation
 {
     public interface IUnsafeValidator
     {
+        /// <summary>
+        /// Check whether the move contains all formulas on the adjacent edges.
+        /// It expects that the move is positionally valid, that is, does not contain a hole in the middle,
+        /// contains only valid positions etc.
+        /// </summary>
+        /// <param name="move"></param>
+        /// <param name="board"></param>
+        /// <param name="formulaIdentifier"></param>
+        /// <returns>True if it is ok.</returns>
+        bool CheckContainedFormulas(Move move, IBoard board, IFormulaIdentifier formulaIdentifier);
     }
 }
