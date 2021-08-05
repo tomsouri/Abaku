1e2202f96ad33c2ca344f7dd5d6f45bac8ae7714
Tomas Sourada <souri.tom@seznam.cz>
2021-08-05 21:16:26 +0200

    Implement IUnsafeEvaluator.

--- Evaluation/IUnsafeEvaluator.cs
+++ Evaluation/IUnsafeEvaluator.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CommonTypes;
+using BoardManaging;
+using OperationsManaging;
+
 namespace Evaluation
 {
     public interface IUnsafeEvaluator
     {
+        /// <summary>
+        /// Evaluates the valid move in the current situation.
+        /// </summary>
+        /// <param name="move"></param>
+        /// <param name="board"></param>
+        /// <param name="formulaIdentifier"></param>
+        /// <returns>The score you get after applying the move.</returns>
+        int EvaluateValidMove(Move move, IBoard board, IFormulaIdentifier formulaIdentifier);
     }
 }
