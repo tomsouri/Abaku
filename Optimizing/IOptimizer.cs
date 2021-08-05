b1571339d37117edace46e3056b19802da95a7f2
Tomas Sourada <souri.tom@seznam.cz>
2021-08-05 20:50:40 +0200

    Add comment to GetBestMove method in IOptimizer.

--- Optimizing/IOptimizer.cs
+++ Optimizing/IOptimizer.cs
@@ -14,6 +14,15 @@ namespace Optimizing
 {
     public interface IOptimizer
     {
+        /// <summary>
+        /// Finds the best move in the current situation.
+        /// </summary>
+        /// <param name="availableDigits">Digits that can be placed in the move.</param>
+        /// <param name="board">Board with the current situation.</param>
+        /// <param name="formulaIdentifier">Identifier to identify formulas.</param>
+        /// <param name="evaluator">Evaluator to get score of the moves.</param>
+        /// <param name="validator">Validator to validate the moves.</param>
+        /// <returns>The move with the highest score.</returns>
         Move GetBestMove(IReadOnlyList<Digit> availableDigits, IExtendedBoard board, IFormulaIdentifier formulaIdentifier, IUnsafeEvaluator evaluator, IUnsafeValidator validator);
     }
 }
