7de5e66c507931421a3f28d820455479604c7788
Tomas Sourada <souri.tom@seznam.cz>
2021-08-06 11:45:20 +0200

    Implement CreateBoard and LoadEmptyCells methods in OptBoard.

--- Optimizing/OptimizerBoard.cs
+++ Optimizing/OptimizerBoard.cs
@@ -15,7 +15,40 @@ namespace Optimizing
     }
     internal class OptimizerBoard// : IOptimizerBoard
     {
+        public OptimizerBoard(IExtendedBoard extendedBoard)
+        {
+            Initialize(extendedBoard);
+        }
+        private void Initialize(IExtendedBoard extendedBoard)
+        {
+            CreateBoard(extendedBoard);
+            LoadEmptyCells(extendedBoard);
+        }
+        private void CreateBoard(IExtendedBoard extendedBoard)
+        {
+            var rowsCount = extendedBoard.RowsCount;
+            var colsCount = extendedBoard.ColumnsCount;
 
+            _board = new Cell[rowsCount][];
+            for (int i = 0; i < rowsCount; i++)
+            {
+                _board[i] = new Cell[colsCount];
+            }
+        }
+        private void LoadEmptyCells(IExtendedBoard extendedBoard)
+        {
+            var rowsCount = extendedBoard.RowsCount;
+            var colsCount = extendedBoard.ColumnsCount;
+
+            for (int rowIndex = 0; rowIndex < rowsCount; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < colsCount; colIndex++)
+                {
+                    var position = new Position(rowIndex, colIndex);
+                    this[position].IsEmpty = extendedBoard.IsPositionEmpty(position);
+                }
+            }
+        }
 
         private Cell[][] _board;
         private Cell this[Position position]
