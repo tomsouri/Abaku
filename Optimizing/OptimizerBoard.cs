a74550816453dc31349df4108463ca7d1873bbb1
Tomas Sourada <souri.tom@seznam.cz>
2021-08-16 17:29:00 +0200

    Make LoadEmptyCells method static.

--- Optimizing/OptimizerBoard.cs
+++ Optimizing/OptimizerBoard.cs
@@ -66,7 +66,7 @@ namespace Optimizing
             // prazdnych a nonadj bunek za
             return board;
         }
-        private void LoadEmptyCells(IExtendedBoard extendedBoard, Board target)
+        private static void LoadEmptyCells(IExtendedBoard extendedBoard, Board target)
         {
             var rowsCount = extendedBoard.RowsCount;
             var colsCount = extendedBoard.ColumnsCount;
