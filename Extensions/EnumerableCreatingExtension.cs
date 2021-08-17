bec90bec65cb7feff3374f04b2c4b6be54647e65
Tomas Sourada <souri.tom@seznam.cz>
2021-08-17 17:23:10 +0200

    Add GetSingleEnumerable extension.

---
+++ Extensions/EnumerableCreatingExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableCreatingExtensions
+{
+    public static class EnumerableCreatingExtension
+    {
+        /// <summary>
+        /// Creates an enumerable containing only one item.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="singleItem"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> GetSingleEnumerable<T>(this T singleItem)
+        {
+            yield return singleItem;
+        }
+    }
+}
