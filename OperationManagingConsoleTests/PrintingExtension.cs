0dac79b4d5e2f3b1de26f9f23a8de7ed7533db3d
Tomas Sourada <souri.tom@seznam.cz>
2021-08-18 16:49:36 +0200

    Update printing extension to print more readably.

--- OperationManagingConsoleTests/PrintingExtension.cs
+++ OperationManagingConsoleTests/PrintingExtension.cs
@@ -13,15 +13,24 @@ namespace InteractiveConsoleTests
         public static void Print(this Digit?[][] board)
         {
             Console.WriteLine("-------------------------------------------------");
+            Console.Write("Column: ");
+            for (int i = 0; i < board[0].Length; i++)
+            {
+                Console.Write("{0} ",i.ToString("D2"));
+            }
+            Console.WriteLine();
+            int index = 0;
             foreach (var row in board)
             {
+                Console.Write("row {0}: ", index.ToString("D2"));
                 foreach (var digit in row)
                 {
-                    Console.Write(digit == null ? "_" : digit);
+                    Console.Write(digit == null ? "__" : " "+digit);
                     Console.Write(" ");
 
                 }
                 Console.WriteLine();
+                index++;
             }
             Console.WriteLine("--------------------------------------------------");
         }
