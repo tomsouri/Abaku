7b10c12ee94f046965c89bde5cf7dd205131cbd7
Tomas Sourada <souri.tom@seznam.cz>
2021-08-18 16:31:16 +0200

    Add Printing extension for printing board content.

---
+++ OperationManagingConsoleTests/PrintingExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveConsoleTests
+{
+    public static class PrintingExtension
+    {
+        public static void Print(this Digit?[][] board)
+        {
+            Console.WriteLine("-------------------------------------------------");
+            foreach (var row in board)
+            {
+                foreach (var digit in row)
+                {
+                    Console.Write(digit == null ? "_" : digit);
+                    Console.Write(" ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------------------");
+        }
+    }
+}
