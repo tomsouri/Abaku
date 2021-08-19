583dd97f6b2eb887202078278e2713aea8f2693f
Tomas Sourada <souri.tom@seznam.cz>
2021-08-19 13:03:14 +0200

    Make RepresentsValidNumber extension method.

--- Extensions/ReadOnlyListDigitExtensions.cs
+++ Extensions/ReadOnlyListDigitExtensions.cs
@@ -65,7 +65,7 @@ namespace ReadOnlyListDigitExtensions
             return (tuple.digits1.ToLong(), tuple.digits2.ToLong(), tuple.digits3.ToLong());
         }
 
-        public static bool RepresentsValidNumber(IReadOnlyList<Digit> digits)
+        public static bool RepresentsValidNumber(this IReadOnlyList<Digit> digits)
         {
             return digits.Count > 0 && digits[0] != Digit.ZERO;
         }
